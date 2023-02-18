682ef9f07e40b2bf82a908e033199a32362563e2
Maciej Rozewicz <rozewicz.maciej@gmail.com>
2023-02-19 00:17:45 +0100

    Updated test

--- tests/model_cv.hpp
+++ tests/model_cv.hpp
@@ -4,6 +4,7 @@
 #include "mem_ekf.hpp"
 
 namespace eot {
+  /* x, y, vx, vy */
   class ModelCv : public MemEkf<4u, 2u> {
     public:
       explicit ModelCv(const MemEkfCalibrations<4u> & calibrations) 
@@ -18,19 +19,20 @@ namespace eot {
 
         // Update kinematic
         state_.kinematic_state.state = transition_matrix_ * state_.kinematic_state.state;
-        state_.kinematic_state.covariance = transition_matrix_ * state_.kinematic_state.covariance * transition_matrix_.transpose() + c_kinematic_;
+        state_.kinematic_state.covariance = transition_matrix_ * state_.kinematic_state.covariance * transition_matrix_.transpose() + time_delta * c_kinematic_;
       }
     
     private:
       void SetTransitionMatrix(const double time_delta) {
+        //x
         transition_matrix_(0u, 0u) = 1.0;
-        transition_matrix_(0u, 1u) = time_delta;
-
-        transition_matrix_(1u, 1u) = 1.0;
-
-        transition_matrix_(2u, 2u) = 1.0;
-        transition_matrix_(2u, 3u) = time_delta;
-
+        transition_matrix_(0u, 2u) = time_delta;
+        // y
+        transition_matrix_(1u, 2u) = 1.0;
+        transition_matrix_(1u, 3u) = time_delta;
+        // vx
+        transition_matrix_(2u, 1u) = 1.0;
+        // vy
         transition_matrix_(3u, 3u) = 1.0;
       }
       StateMatrix transition_matrix_ = StateMatrix::Zero();
